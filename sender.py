392b8bcfb01d4934f1981a37ad42782e971ef24f
Tamunosaki Wokoma <tamunosakiwokoma86@gmail.com>
2024-08-15 21:08:41 -0600

    Update sender.py
    Import necessary modules

--- sender.py
+++ sender.py
@@ -1,1 +1,10 @@
-
+"""
+Python script designed to send an email with an optional file attachment
+"""
+#Import necessary modules
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+import os
